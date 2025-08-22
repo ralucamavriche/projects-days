d8f98914a2886161e9b2768bd5ced81518b93eb4
ralucamavriche <ralucamavriche@gmail.com>
2025-08-22 13:12:14 +0300

    feat: Rebuild expanding cards project with new HTML, CSS, and JavaScript files

---
+++ expanding-cards-project/js/main.js
@@ -0,0 +1,8 @@
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        cards.forEach(c => c.classList.remove('expanded'));
+        card.classList.add('expanded')
+    })
+})
